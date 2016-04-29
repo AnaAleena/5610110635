f387f01d20833cc1dff980f45d6a6f5a753f6673
AnaAleena <aleena.ana22@gmail.com>
2016-04-29 09:14:46 +0700

    #03,#04
    ทำให้ยานเคลื่อนไหวตามปุ่มกดซ้าย-ขวาได้ , เพิ่ม Enemy
    สีแดงให้หล่นลงมาจากฟ้า โดยเมื่อชนกับยาน SpaceShip
    ยังไม่เกิดอะไรขึ้น

--- GameEngine.java
+++ GameEngine.java
@@ -1,36 +1,95 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
+import javax.swing.Timer;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+public class GameEngine implements KeyListener{
+		GamePanel gp;
 
-public class GameEngine {
-	GamePanel gp;
-	
-		
-		private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
-		private SpaceShip v;
-		
-		//private Timer timer;
-		private double difficulty = 0.1;
+		private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+		private SpaceShip v;	
 
-	public GameEngine(GamePanel gp, SpaceShip v) {
-		this.gp = gp;
-		this.v = v;		
+		private Timer timer;
+
+		public GameEngine(GamePanel gp, SpaceShip v) {
+			this.gp = gp;
+			this.v = v;		
 		
-		gp.sprites.add(v);
-		gp.updateGameUI();
-		
-	}	private void generateEnemy(){
-		Enemy e = new Enemy((int)(Math.random()*390), 30);
-		gp.sprites.add(e);
-		enemies.add(e);
-	}
-		private void process(){
-		if(Math.random() < difficulty){
-			generateEnemy();
+			gp.sprites.add(v);
+			
+			timer = new Timer(50, new ActionListener() {
+			
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					process();
+				}
+			});
+			timer.setRepeats(true);
+
 		}
-}}
+
+		public void start(){
+			timer.start();
+		}
+
+		private void generateEnemy(){
+			Enemy e = new Enemy((int)(Math.random()*390), 30);
+			gp.sprites.add(e);
+			enemies.add(e);
+		}
+
+		private void process(){
+			generateEnemy();
+
+			Iterator<Enemy> e_iter = enemies.iterator();
+			while(e_iter.hasNext()){
+				Enemy e = e_iter.next();
+				e.proceed();
+			
+				if(!e.isAlive()){
+					e_iter.remove();
+					gp.sprites.remove(e);
+				}
+			}
+		
+			gp.updateGameUI();
+
+		}
+
+		public void die(){
+			timer.stop();
+		}
+
+		void controlVehicle(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				v.move(-1);
+				break;
+			case KeyEvent.VK_RIGHT:
+				v.move(1);
+				break;
+			}
+		}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+	
+}
